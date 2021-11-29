891a3c4bc2103eb49cc1400d26589eabec53d1af
Smitpatel2000 <41635586+Smitpatel2000@users.noreply.github.com>
2021-11-29 10:02:23 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,37 @@
-import face_recognition
-from flask import Flask, jsonify, request
-import json 
-app = Flask(__name__)
+from flask import Flask,render_template,Response
+import cv2
+
+app=Flask(__name__)
+camera=cv2.VideoCapture("sample.mp4")
+
+def generate_frames():
+    while True:
+            
+        ## read the camera frame
+        success,frame=camera.read()
+        body_classifier = cv2.CascadeClassifier('haarcascade_fullbody.xml')         
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        bodies = body_classifier.detectMultiScale(gray, 1.2, 3)
+        for (x,y,w,h) in bodies:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+                
+        if not success:
+            break
+        else:
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 
 @app.route('/')
-def welcome():
-    return "Welcome to khoj project - face recognizer"
+def index():
+    return render_template('index.html')
 
-@app.route('/recognizeface', methods=['POST'])
-def recognizeface():     
-    face1 = request.files['face1']
-    face2 = request.files['face2']    
-    known_image = face_recognition.load_image_file(face1)
-    unknown_image = face_recognition.load_image_file(face2)
-    biden_encoding = face_recognition.face_encodings(known_image)[0]
-    unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
-    results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
-    return str(results)
+@app.route('/video')
+def video():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == '__main__':
+if __name__=="__main__":
     app.run()
